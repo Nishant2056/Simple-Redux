1ecd7697fc7035106f354693cff9bb79ce1982a3
nishant bhurtel <bhurtelnishant@gmail.com>
2025-12-30 12:10:35 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addTask, removeTask } from "./workSlice";
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState("");
+  const tasks = useSelector((state) => state.work.tasks);
+  const dispatch = useDispatch();
+
+  const handleAdd = () => {
+    dispatch(addTask(input));
+    setInput("");
+  };
+
+  return (
+    <>
+      <h1>Redux Task Manager</h1>
+      <div className="card">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter a new task"
+        />
+        <button onClick={handleAdd}>Add Task</button>
+      </div>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        {tasks.map((task, index) => (
+          <li
+            key={index}
+            style={{
+              margin: "10px 0",
+              display: "flex",
+              justifyContent: "center",
+              gap: "10px",
+              alignItems: "center",
+            }}
+          >
+            <span>{task}</span>
+            <button onClick={() => dispatch(removeTask(index))}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
---
+++ src/workSlice.jsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const workSlice = createSlice({
+  name: "work",
+  initialState: { tasks: [] },
+  reducers: {
+    addTask: (state, action) => {
+      state.tasks.push(action.payload);
+    },
+    removeTask: (state, action) => {
+      state.tasks = state.tasks.filter(
+        (task, index) => index !== action.payload
+      );
+    },
+  },
+});
+
+export const { addTask, removeTask } = workSlice.actions;
+export default workSlice.reducer;
